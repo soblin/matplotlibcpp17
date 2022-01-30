c2c5d8ba7f3d1d1a9dabff8141043e4a5daa1eeb
soblin <hilo.soblin@gmail.com>
2022-01-31 02:12:44 +0900

    gs(r,c) now returns SubplotSpec

--- include/matplotlibcpp17/gridspec.h
+++ include/matplotlibcpp17/gridspec.h
@@ -1,5 +1,11 @@
 namespace gridspec {
 
+struct __attribute__((visibility("hidden"))) SubplotSpec {
+  SubplotSpec(pybind11::object subplotspec) { self = subplotspec; }
+  pybind11::object self;
+  pybind11::object unwrap() { return self; }
+};
+
 struct __attribute__((visibility("hidden"))) GridSpec {
   GridSpec(int nrow_, int ncol_) {
     nrow = nrow_;
@@ -11,19 +17,10 @@ struct __attribute__((visibility("hidden"))) GridSpec {
         pybind11::module::import("matplotlib.gridspec").attr("GridSpec");
     self = gridspec_attr(nrow, ncol);
   }
-  pybind11::object operator()(int r, int c) {
-    // if r == -1 or c == -1, do slice
-    if (r == -1) {
-      if (c == -1) {
-        return self;
-      }
-      auto rs = pybind11::slice(0, nrow, 1);
-      return self[pybind11::make_tuple(rs, c)];
-    } else if (c == -1) {
-      auto cs = pybind11::slice(0, ncol, 1);
-      return self[pybind11::make_tuple(r, cs)];
-    } else
-      return self[pybind11::make_tuple(r, c)];
+  template <typename Rows, typename Cols>
+  SubplotSpec operator()(const Rows &r, const Cols &c) {
+    pybind11::object obj = self[pybind11::make_tuple(r, c)];
+    return SubplotSpec(obj);
   }
   int nrow, ncol;
   pybind11::object self;
