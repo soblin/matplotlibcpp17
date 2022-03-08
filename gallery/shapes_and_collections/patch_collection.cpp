4b39834ce8e4edacaeb0dc478aee5cd503887eeb
MamoruSobue <mamoru.sobue@jpl.nasa.gov>
2022-03-09 07:36:30 +0900

    updated gallery/shapes_and_collections/patch_collection.cpp

--- gallery/shapes_and_collections/patch_collection.cpp
+++ gallery/shapes_and_collections/patch_collection.cpp
@@ -3,18 +3,19 @@
 
 #include <pybind11/embed.h>
 #include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
 #include <matplotlibcpp17/pyplot.h>
 #include <matplotlibcpp17/patches.h>
 
-#include <algorithm>
-#include <iostream>
+#include <xtensor/xrandom.hpp>
+
 #include <vector>
 
 namespace py = pybind11;
 using namespace py::literals;
 using namespace std;
-using namespace matplotlibcpp17;
+using namespace matplotlibcpp17::patches;
 using namespace matplotlibcpp17;
 
 int main() {
@@ -22,27 +23,30 @@ int main() {
   auto plt = matplotlibcpp17::pyplot::import();
   auto [fig, ax] = plt.subplots();
 
+  const int resolution = 50;
   const int N = 3;
-  vector<double> x = {0.7003673, 0.74275081, 0.70928001},
-                 y = {0.56674552, 0.97778533, 0.70633485},
-                 radii = {0.02479158, 0.01578834, 0.06976985};
+  xt::xarray<double> x = xt::random::rand<double>({N});
+  xt::xarray<double> y = xt::random::rand<double>({N});
+  xt::xarray<double> radii = 0.1 * xt::random::rand<double>({N});
   py::list patches; // instead of patches = []
   for (int i = 0; i < N; ++i) {
     const double x1 = x[i], y1 = y[i], r = radii[i];
     auto circle = patches::Circle(Args(py::make_tuple(x1, y1), r));
     patches.append(circle.unwrap());
   }
-  x = {0.71995667, 0.25774443, 0.34154678};
-  y = {0.96876117, 0.6945071, 0.46638326};
-  radii = {0.07028127, 0.05117859, 0.09287414};
-  vector<double> theta1 = {266.3169476, 224.07805212, 234.5563688},
-                 theta2 = {142.85074015, 195.56618216, 287.96383014};
+
+  x = xt::random::rand<double>({N});
+  y = xt::random::rand<double>({N});
+  radii = 0.1 * xt::random::rand<double>({N});
+  xt::xarray<double> theta1 = 360.0 * xt::random::rand<double>({N});
+  xt::xarray<double> theta2 = 360.0 * xt::random::rand<double>({N});
   for (int i = 0; i < N; ++i) {
     const double x1 = x[i], y1 = y[i], r = radii[i], th1 = theta1[i],
                  th2 = theta2[i];
     auto wedge = patches::Wedge(Args(py::make_tuple(x1, y1), r, th1, th2));
     patches.append(wedge.unwrap());
   }
+
   patches.append(
       patches::Wedge(Args(py::make_tuple(0.3, 0.7), 0.1, 0, 360)).unwrap());
   patches.append(patches::Wedge(Args(py::make_tuple(0.7, 0.8), 0.2, 0, 360),
@@ -53,16 +57,30 @@ int main() {
   patches.append(patches::Wedge(Args(py::make_tuple(0.8, 0.3), 0.2, 45, 90),
                                 Kwargs("width"_a = 0.10))
                      .unwrap());
-  // NOTE: Polygon take numpy array as argument, so skip it
-  vector<double> colors_ = {90.63036451, 16.10182093, 74.36211347, 63.29741618,
-                            32.41800177, 92.23765324, 23.72264387, 82.39455709,
-                            75.06071403, 11.37844527};
-  py::list colors = py::cast(colors_);
+
+  for (int i = 0; i < N; ++i) {
+    auto poly__ = xt::random::rand<double>({N, 2});
+    vector<vector<double>> poly_(N);
+    // to vector<vector>>
+    for (int j = 0; j < N; ++j) {
+      poly_[j].resize(2);
+      poly_[j][0] = poly__(j, 0);
+      poly_[j][1] = poly__(j, 1);
+    }
+    // to numpy array
+    auto poly = py::array(py::cast(std::move(poly_)));
+    auto polygon = Polygon(Args(poly, true));
+    patches.append(polygon.unwrap());
+  }
+
+  auto colors__ = 100.0 * xt::random::rand<double>({patches.size()});
+  vector<double> colors_(colors__.begin(), colors__.end());
+  py::array colors = py::cast(colors_);
   auto p = collections::PatchCollection(Args(patches), Kwargs("alpha"_a = 0.4));
   p.set_array(Args(colors));
-  // NOTE: error in python3.6.9 ?
   ax.add_collection(Args(p.unwrap()));
   fig.colorbar(Args(p.unwrap()), Kwargs("ax"_a = ax.unwrap()));
+
 #if USE_GUI
   plt.show();
 #else
